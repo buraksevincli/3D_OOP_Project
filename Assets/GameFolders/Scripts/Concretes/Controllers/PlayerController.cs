6f851db7f82e991983033774fd1b9b95f05b0115
Burak Sevincli <brksvncli@gmail.com>
2023-04-30 22:08:26 +0300

    ProjectCreated

---
+++ Assets/GameFolders/Scripts/Concretes/Controllers/PlayerController.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace GameFolders.Scripts.Concretes.Controllers
+{
+    public class PlayerController : MonoBehaviour
+    {
+        
+    }
+}
+
