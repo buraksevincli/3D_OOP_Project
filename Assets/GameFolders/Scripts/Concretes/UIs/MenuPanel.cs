2f564e457cccbc4fc2f880f8b4c72b7c3f30b1e9
Burak Sevincli <brksvncli@gmail.com>
2023-05-01 04:51:48 +0300

    Menu Scene Added

---
+++ Assets/GameFolders/Scripts/Concretes/UIs/MenuPanel.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using GameFolders.Scripts.Concretes.Managers;
+using UnityEngine;
+
+namespace GameFolders.Scripts.Concretes.UIs
+{
+    public class MenuPanel : MonoBehaviour
+    {
+        public void StartClicked()
+        {
+            GameManager.Instance.LoadLevelScene(1);
+        }
+
+        public void ExitClicked()
+        {
+            GameManager.Instance.Exit();
+        }
+    }
+}
+
